1e2696f2addf0de22b560b58a55b14b5179f084d
Nadimandpicaroon <hasamahmudnadim5656@gmail.com>
2024-09-24 20:56:59 +0600

    added a modal

--- js/donate.js
+++ js/donate.js
@@ -1,38 +1,73 @@
-// document.getElementById('donate-btn-noakhali').addEventListener('click', function (event) {
-//     event.preventDefault
-
-//     const donationNoakhali = getInputFieldValueById('donation-noakhali');
-//     if(donationNoakhali === parseFloat){
-//         const balanceNoakhali = getTextFieldValueById('balance-noakhali');
-//         const newBalanceNoakhali = balanceNoakhali + donationNoakhali;
-//         document.getElementById('balance-noakhali').innerText = newBalanceNoakhali;
-        // alert('Failed to donate the money');
-//     }
-//     else{
-//         alert('Failed to donate the money');
-//         // const balanceNoakhali = getTextFieldValueById('balance-noakhali');
-//         // const newBalanceNoakhali = balanceNoakhali + donationNoakhali;
-//         // document.getElementById('balance-noakhali').innerText = newBalanceNoakhali;
-//     }
-// })
-
 // donation button tab
 document.getElementById('donation-tab').addEventListener('click', function(){
     const main = document.getElementById('main');
     main.classList.remove('hidden');
 })
 
-
 // add money in the main section
+
+// noakhali part
+
 document.getElementById('donate-btn-noakhali').addEventListener('click', function(){
+
     const donateNoakhali = parseFloat(document.getElementById('donate-noakhali').value);
     const balanceNoakhali = parseFloat(document.getElementById('balance-noakhali').innerText);
+    const myCurrentBalance = parseFloat(document.getElementById('my-current-balance').innerText);
+
    const newBalanceNoakhali = donateNoakhali + balanceNoakhali;
-   document.getElementById('balance-noakhali').innerText = newBalanceNoakhali;
+
+   const newCurrentBalance = myCurrentBalance - donateNoakhali;
+   
    if(donateNoakhali <= 0 || isNaN(donateNoakhali) || newBalanceNoakhali < donateNoakhali){
     alert('invalid input');
     return;
    }
+
+   document.getElementById('balance-noakhali').innerText = newBalanceNoakhali;
+   document.getElementById('my-current-balance').innerText = newCurrentBalance;
+})
+
+// feni part
+
+document.getElementById('donate-btn-feni').addEventListener('click', function(event){
+     event.preventDefault();
+
+    const donateFeni = parseFloat(document.getElementById('donate-feni').value);
+    const balanceFeni = parseFloat(document.getElementById('balance-feni').innerText);
+    const myCurrentBalance = parseFloat(document.getElementById('my-current-balance').innerText);
+
+   const newBalanceFeni = donateFeni + balanceFeni;
+
+   const newCurrentBalance = myCurrentBalance - donateFeni;
+   
+   if(donateFeni <= 0 || isNaN(donateFeni) || newBalanceFeni < donateFeni){
+    alert('invalid input');
+    return;
+   }
+
+   document.getElementById('balance-feni').innerText = newBalanceFeni;
+   document.getElementById('my-current-balance').innerText = newCurrentBalance;
+})
+
+// quota part 
+
+document.getElementById('donate-btn-qouta').addEventListener('click', function(){
+
+    const donateQouta = parseFloat(document.getElementById('donate-qouta').value);
+    const balanceQouta = parseFloat(document.getElementById('balance-qouta').innerText);
+    const myCurrentBalance = parseFloat(document.getElementById('my-current-balance').innerText);
+
+   const newBalanceQouta = donateQouta + balanceQouta;
+
+   const newCurrentBalance = myCurrentBalance - donateQouta;
+   
+   if(donateQouta <= 0 || isNaN(donateQouta) || newBalanceQouta < donateQouta){
+    alert('invalid input');
+    return;
+   }
+
+   document.getElementById('balance-qouta').innerText = newBalanceQouta;
+   document.getElementById('my-current-balance').innerText = newCurrentBalance;
 })
 
 
