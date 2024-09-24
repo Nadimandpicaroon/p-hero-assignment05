48c97e919983f1e2d602c5d60f890a6e804121a5
Nadimandpicaroon <hasamahmudnadim5656@gmail.com>
2024-09-24 13:24:24 +0600

    added a new html file

--- js/donate.js
+++ js/donate.js
@@ -16,11 +16,40 @@
 //     }
 // })
 
+// donation button tab
 document.getElementById('donation-tab').addEventListener('click', function(){
     const main = document.getElementById('main');
     main.classList.remove('hidden');
 })
 
-// document.getElementById('donate-btn-noakhali').addEventListener('click', function(){
-//     const donateNoakhali = document.getElementById('donate-input-noakhali');
-// })
+
+// add money in the main section
+document.getElementById('donate-btn-noakhali').addEventListener('click', function(){
+    const donateNoakhali = parseFloat(document.getElementById('donate-noakhali').value);
+    const balanceNoakhali = parseFloat(document.getElementById('balance-noakhali').innerText);
+   const newBalanceNoakhali = donateNoakhali + balanceNoakhali;
+   document.getElementById('balance-noakhali').innerText = newBalanceNoakhali;
+   if(donateNoakhali <= 0 || isNaN(donateNoakhali) || newBalanceNoakhali < donateNoakhali){
+    alert('invalid input');
+    console.log(balanceNoakhali);
+    return;
+   }
+})
+
+
+// history button tab 
+const historyTab = document.getElementById('history-tab');
+const donationTab = document.getElementById('donation-tab');
+historyTab.addEventListener('click', function(){
+    historyTab.classList.add(
+        "bg-[rgba(180,244,97,1)]",
+        "hover:bg-lime-400" ,
+        "transition"
+    );
+
+    donationTab.classList.remove(
+      "bg-[rgba(180,244,97,1)]",
+      "hover:bg-lime-400",
+      "transition"  
+    );
+})
